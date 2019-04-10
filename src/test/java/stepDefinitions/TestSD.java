74cecccf651807fd452569f58264abd5ce8995b4
uddasari <cg.gangula@gmail.com>
2019-04-09 21:13:46 -0500

    Implemented feature file and step definition files

--- src/test/java/stepDefinitions/TestSD.java
+++ src/test/java/stepDefinitions/TestSD.java
@@ -1,38 +1,80 @@
 package stepDefinitions;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 /**
- * Unit test for simple App.
+ * Step definition for the feature file 
  */
-public class TestSD 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public TestSD( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( TestSD.class );
-    }
+public class TestSD {
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Given("^some other precondition$")
+	public void some_other_precondition() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I complete action$")
+	public void i_complete_action() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^some other action$")
+	public void some_other_action() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^yet another action$")
+	public void yet_another_action() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I validate the outcomes$")
+	public void i_validate_the_outcomes() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^check more outcomes$")
+	public void check_more_outcomes() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I want to write a step with name(\\d+)$")
+	public void i_want_to_write_a_step_with_name(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I check for the (\\d+) in step$")
+	public void i_check_for_the_in_step(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I verify the success in step$")
+	public void i_verify_the_success_in_step() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I verify the Fail in step$")
+	public void i_verify_the_Fail_in_step() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
 }
